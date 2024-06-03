74c048cf1de508f10bca25a22499ad9e80910e26
unknown <01aniket26@gmail.com>
2024-06-03 19:57:24 +0530

    fixes

--- templateRun_cmds.ps1
+++ templateRun_cmds.ps1
@@ -1,8 +1,15 @@
 ﻿$resourceGroupName = "demo"  
 $deploymentName = "storage-deployment"  
-$storageAccountName = "task1storage"  
+$storageAccountName = "aniketstorage"  
 $containerName = "backupdata"  
   
+New-AzResourceGroup -name $resourceGroupName -location "EASTUS"
   
-  
-New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateUri "https://raw.githubusercontent.com/An1ket26/ps_codes/master/storage-template.json" -TemplateParameterObject @{"storageAccountName" = $storageAccountName "containerName" = $containerName}  
+New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName `
+-TemplateFile "C:\Users\aniket\Desktop\ps_cmds\storage-template.json" `
+-TemplateParameterObject @{"storageAccountName" = $storageAccountName 
+"containerName" = $containerName}  
+
+$storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -AccountName $storageAccountName).Value[0]  
+$storageContext = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey  
+Set-AzStorageBlobContent -Context $storageContext -Container $containerName -File "C:\Users\aniket\Desktop\ps_cmds\sql_backup.txt" -Blob "blob1"
