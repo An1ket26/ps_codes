1a95b3d11aa7a515e4d1e7c0fb15ceed8794808f
unknown <01aniket26@gmail.com>
2024-06-03 19:17:37 +0530

    added template for storage account and container

---
+++ templateRun_cmds.ps1
@@ -0,0 +1,13 @@
+﻿$resourceGroupName = "demo"  
+$deploymentName = "storage-deployment"  
+$storageAccountName = "task1storage"  
+$containerName = "backupdata"  
+  
+  
+  
+New-AzResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName `  
+  -TemplateUri "storage-template.json" `  
+  -TemplateParameterObject @{  
+    "storageAccountName" = $storageAccountName  
+    "containerName" = $containerName  
+  }  
